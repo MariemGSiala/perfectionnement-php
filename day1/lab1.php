3498fce10bb86e36d7c818e0d8361e6b60ebbbd4
Mariem <gharbi.mariem92@gmail.com>
2022-11-14 10:50:46 +0100

    message

--- day1/lab1.php
+++ day1/lab1.php
@@ -1,6 +1,7 @@
 <?php
 echo "<h1> l'orientée objet</h1>";
-echo "<h2> Part1: les classes, les méthodes et les attributs </h2>";
+echo "<h2> Part1: les classes, les méthodes, attributs d'instance et de classe</h2>";
 echo "<h2> Part2: les modificateurs de visibilité </h2>";
-echo "<h2> Part3: les modificateurs de visibilité </h2>";
+echo "<h2> Part3: l'héritage </h2>";
+echo "<h2> Part4: les classes abstraites et interfacialles </h2>";
 ?>
