d060d9f9a862491c06f01628f46730d20095f301
Mariem <gharbi.mariem92@gmail.com>
2022-11-14 10:42:35 +0100

    message

---
+++ day1/lab1.php
@@ -0,0 +1,6 @@
+<?php
+echo "<h1> l'orientée objet</h1>";
+echo "<h2> Part1: les classes, les méthodes et les attributs </h2>";
+echo "<h2> Part2: les modificateurs de visibilité </h2>";
+echo "<h2> Part3: les modificateurs de visibilité </h2>";
+?>
